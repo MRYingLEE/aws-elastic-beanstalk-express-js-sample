3f7e2628c319ac04070e7bc440b94b9dc951fd28
Cenv HK <mr.ying.lee@gmail.com>
2020-09-28 11:11:50 +0800

    cicd test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Ying!'));
+app.get('/', (req, res) => res.send('Hello Ying2!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
